aa2550d964508ac432ad3d35ba8fb5d4abfcf0a1
Andrew Ferreira <andrew.shien2@gmail.com>
2025-04-03 16:17:05 -0300

    feat: add size helpers

--- src/composables/element-size/index.ts
+++ src/composables/element-size/index.ts
@@ -1,4 +1,5 @@
-import { type MaybeRefOrGetter, type Ref, toRef } from 'vue';
+import { useWindowHeight, useWindowWidth } from '../window-size';
+import { computed, type MaybeRefOrGetter, type Ref, toRef } from 'vue';
 import { type MaybeElement, useElementSize as original } from '@vueuse/core';
 
 export function useElementSize<T extends MaybeElement>(element: MaybeRefOrGetter<T>) {
@@ -9,6 +10,18 @@ export function useHeight<T extends MaybeElement>(element: MaybeRefOrGetter<T>) 
   return useElementSize(element).height as Readonly<Ref<number>>;
 }
 
+export function useHeightDiff<T extends MaybeElement>(element: MaybeRefOrGetter<T>) {
+  const height = useHeight(element);
+  const windowHeight = useWindowHeight();
+  return computed(() => windowHeight.value - height.value);
+}
+
 export function useWidth<T extends MaybeElement>(element: MaybeRefOrGetter<T>) {
   return useElementSize(element).width as Readonly<Ref<number>>;
+}
+
+export function useWidthDiff<T extends MaybeElement>(element: MaybeRefOrGetter<T>) {
+  const width = useWidth(element);
+  const windowWidth = useWindowWidth();
+  return computed(() => windowWidth.value - width.value);
 }
