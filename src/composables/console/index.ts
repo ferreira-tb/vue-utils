8fa764e7108989ab836f5cc9293dcd5323cb6cbd
Andrew Ferreira <andrew.shien2@gmail.com>
2025-07-26 15:28:23 -0300

    BREAKING CHANGE: reactive console

--- src/composables/console/index.ts
+++ src/composables/console/index.ts
@@ -1,9 +1,43 @@
-import { watch, type WatchSource } from 'vue';
+import { toRef } from 'vue';
+import { watchImmediate } from '@vueuse/core';
+import type { MaybeNilRef } from '../../types';
 
-export function log<T>(source: WatchSource<T>) {
-  return watch(source, (value) => console.log(value));
+export const console = {
+  assert,
+  debug,
+  error,
+  info,
+  log,
+  trace,
+  warn,
+} as const;
+
+function assert(cond: MaybeNilRef<boolean>, message: string) {
+  return watchImmediate(toRef(cond), (value) => {
+    globalThis.console.assert(value ?? false, message);
+  });
 }
 
-export function warn<T>(source: WatchSource<T>) {
-  return watch(source, (value) => console.warn(value));
+function debug<T>(source: MaybeNilRef<T>) {
+  return watchImmediate(toRef(source), (value) => globalThis.console.debug(value));
+}
+
+function error<T>(source: MaybeNilRef<T>) {
+  return watchImmediate(toRef(source), (value) => globalThis.console.error(value));
+}
+
+function info<T>(source: MaybeNilRef<T>) {
+  return watchImmediate(toRef(source), (value) => globalThis.console.info(value));
+}
+
+function log<T>(source: MaybeNilRef<T>) {
+  return watchImmediate(toRef(source), (value) => globalThis.console.log(value));
+}
+
+function trace<T>(source: MaybeNilRef<T>) {
+  return watchImmediate(toRef(source), (value) => globalThis.console.trace(value));
+}
+
+function warn<T>(source: MaybeNilRef<T>) {
+  return watchImmediate(toRef(source), (value) => globalThis.console.warn(value));
 }
