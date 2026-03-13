8ffe2083da6dfd073ce985b7912b0868e980eae2
Andrew Ferreira <andrew.shien2@gmail.com>
2026-03-13 11:48:12 -0300

    chore: update dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [dts({ rollupTypes: false })],
   build: {
-    target: 'esnext',
     outDir: 'dist',
     emptyOutDir: true,
     minify: false,
